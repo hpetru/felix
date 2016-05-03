059056790b97fe9b2541801ea697cec2e46655f3
Petru Hincu <hincupetru@gmail.com>
2016-05-03 14:49:43 +0300

    load default strategies

--- app/assets/javascripts/components/FlexyTable/ajax/columnFetcher.js
+++ app/assets/javascripts/components/FlexyTable/ajax/columnFetcher.js
@@ -2,8 +2,13 @@ import React from 'react';
 import $ from 'jquery';
 
 function columnFetcher({ columnStrategySlug, tableStrategySlug, onSuccess, strategyInputs }) {
+<<<<<<< 143c1f877298ca576401848e67eda6940873046b
   const method = 'GET';
   const url = `/api/v0/flexy_table`;
+=======
+  const httpMethod = 'GET';
+  const apiEndpoint = `/api/v0/flexy_table`;
+>>>>>>> load default strategies
   const data = {
     table_strategy_slug: tableStrategySlug,
     column_strategy_slug: columnStrategySlug,
@@ -12,16 +17,26 @@ function columnFetcher({ columnStrategySlug, tableStrategySlug, onSuccess, strat
 
 
   $.ajax({
+<<<<<<< 143c1f877298ca576401848e67eda6940873046b
     url,
     method,
     data,
     success: onSuccess,
     error: () => {
       console.log('Failed to load data');
+=======
+    url: apiEndpoint,
+    method: httpMethod,
+    data: data,
+    success: onSuccess,
+    error: () => {
+      console.logger('Failed to load data');
+>>>>>>> load default strategies
     }
   });
 }
 
+<<<<<<< 143c1f877298ca576401848e67eda6940873046b
 columnFetcher.propTypes = {
   strategyInputs: React.PropTypes.object.isRequired,
   onSuccess: React.PropTypes.func.isRequired,
@@ -29,4 +44,6 @@ columnFetcher.propTypes = {
   tableStrategySlug: React.PropTypes.string.isRequired,
 };
 
+=======
+>>>>>>> load default strategies
 module.exports = columnFetcher;
