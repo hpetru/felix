0efddcffabf000ccdd2a8c098c57c1599e98a768
Petru Hincu <hincupetru@gmail.com>
2016-05-04 10:27:49 +0300

    backend spike

--- lib/flexy_table/student_groups.rb
+++ lib/flexy_table/student_groups.rb
@@ -1,9 +1,12 @@
 module FlexyTable
   module StudentGroups
+<<<<<<< be8d20990fe23644f22e7b84a504c355a585ee92
     STRATEGIES = [
       StudentGroups::CurrentGrade
     ].freeze
 
+=======
+>>>>>>> backend spike
     def self.table_strategy_slug
       'student_groups'
     end
