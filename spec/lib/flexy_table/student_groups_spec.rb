53806e0aae20c663f378919caa1bbcd522098c1f
Petru Hincu <hincupetru@gmail.com>
2016-05-04 10:30:24 +0300

    hound

--- spec/lib/flexy_table/student_groups_spec.rb
+++ spec/lib/flexy_table/student_groups_spec.rb
@@ -11,7 +11,6 @@ describe FlexyTable::StudentGroups do
 
   describe '.default_columns' do
     it do
-<<<<<<< be8d20990fe23644f22e7b84a504c355a585ee92
       expect(described_class.default_columns).to eq(
         [
           {
@@ -32,26 +31,6 @@ describe FlexyTable::StudentGroups do
           }
         ]
       )
-=======
-      expect(described_class.default_columns).to eq([
-        {
-          strategySlug: 'current_grade',
-          strategyInputs: {}
-        },
-        {
-          strategySlug: 'letter',
-          strategyInputs: {}
-        },
-        {
-          strategySlug: 'main_teacher_name',
-          strategyInputs: {}
-        },
-        {
-          strategySlug: 'students_count',
-          strategyInputs: {}
-        }
-      ])
->>>>>>> backend spike
     end
   end
 end
