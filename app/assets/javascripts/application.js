7274e2e8fe88346c701f7d93b7b73daaf42c6c11
Petru Hincu <hincupetru@gmail.com>
2016-05-16 12:15:43 +0300

    remove all useless code

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,10 +1,3 @@
-//= require components
-//= require react_ujs
-//
 //= require jquery/jquery-2.1.1.js
 //= require bootstrap-sprockets
-//= require metisMenu/jquery.metisMenu.js
-//= require pace/pace.min.js
-//= require slimscroll/jquery.slimscroll.min.js
 //= require_tree .
-//= require iCheck/icheck.min.js
