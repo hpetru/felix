3ae65d7c87c46ac1dd31f974c83421ae66262ab1
Petru Hincu <hincupetru@gmail.com>
2016-05-19 16:30:13 +0300

    thesis grades

--- spec/features/flexy_table_spec.rb
+++ spec/features/flexy_table_spec.rb
@@ -8,17 +8,13 @@ feature 'Flexy table' do
 
     def perform
       grade.value = grade_value
-      grade.save
+      grade.save!
     end
 
     private
 
     def grade
-      @grade ||= StudentSemesterGrade.find_or_initialize_by(
-        student: student,
-        subject: subject,
-        semester: semester
-      )
+      @grade ||= fetch_grade
     end
 
     def student
@@ -37,6 +33,25 @@ feature 'Flexy table' do
 
     def grade_value
       @inputs['grade_value']
+    end
+
+    def fetch_grade
+      case @inputs['grade_type']
+      when 'semester_grade'
+        StudentSemesterGrade.find_or_initialize_by(
+          student: student,
+          subject: subject,
+          semester: semester
+        )
+      when 'thesis_grade'
+        StudentThesisGrade.find_or_initialize_by(
+          student: student,
+          subject: subject,
+          semester: semester
+        )
+      else
+        raise
+      end
     end
   end
 
@@ -81,6 +96,50 @@ feature 'Flexy table' do
 
       expect(StudentSemesterGrade.count).to eq(1)
       expect(grade.value).to eq(9.4)
+    end
+  end
+
+  context 'thesis grade' do
+    before do
+      @student = create(:student)
+      @subject = create(:subject)
+      @semester = create(:semester)
+      @inputs = {
+        'student_id' => @student.id,
+        'subject_id' => @subject.id,
+        'grade_value' => 9,
+        'grade_type' => 'thesis_grade',
+        'grade_inputs' => {
+          'semester_id' => @semester.id
+        }
+      }
+    end
+
+    it 'create a new grade' do
+      builder = GradeBuilder.new(@inputs)
+      builder.perform
+      grade = StudentThesisGrade.first
+
+      expect(StudentThesisGrade.count).to eq(1)
+      expect(grade.value).to eq(9)
+      expect(grade.student).to eq(@student)
+      expect(grade.subject).to eq(@subject)
+    end
+
+    it 'update existent grade' do
+      grade = create(
+        :student_thesis_grade,
+        student: @student,
+        semester: @semester,
+        subject: @subject,
+        value: 8
+      )
+      builder = GradeBuilder.new(@inputs)
+      builder.perform
+      grade.reload
+
+      expect(StudentThesisGrade.count).to eq(1)
+      expect(grade.value).to eq(9)
     end
   end
 end
