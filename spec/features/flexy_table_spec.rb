51b1d5c0c76a3742a262efaf68577b3d5086562a
Petru Hincu <hincupetru@gmail.com>
2016-05-19 16:17:43 +0300

    grade builder

--- spec/features/flexy_table_spec.rb
+++ spec/features/flexy_table_spec.rb
@@ -1,21 +1,25 @@
 require 'rails_helper'
 
 feature 'Flexy table' do
-  class Builder
+  class GradeBuilder
     def initialize(inputs)
       @inputs = inputs
     end
 
     def perform
-      StudentSemesterGrade.create!(
-        student: student,
-        subject: subject,
-        value: @inputs['grade_value'],
-        semester: semester
-      )
+      grade.value = grade_value
+      grade.save
     end
 
     private
+
+    def grade
+      @grade ||= StudentSemesterGrade.find_or_initialize_by(
+        student: student,
+        subject: subject,
+        semester: semester
+      )
+    end
 
     def student
       Student.find(@inputs['student_id'])
@@ -30,25 +34,53 @@ feature 'Flexy table' do
         @inputs['grade_inputs']['semester_id']
       )
     end
+
+    def grade_value
+      @inputs['grade_value']
+    end
   end
 
-  it 'semester grade' do
-    student = create(:student)
-    subject = create(:subject)
-    semester = create(:semester)
-    inputs = {
-      'student_id' => student.id,
-      'subject_id' => subject.id,
-      'grade_value' => 10,
-      'grade_type' => 'semester_grade',
-      'grade_inputs' => {
-        'semester_id' => semester.id
+  context 'semester grade' do
+    before do
+      @student = create(:student)
+      @subject = create(:subject)
+      @semester = create(:semester)
+      @inputs = {
+        'student_id' => @student.id,
+        'subject_id' => @subject.id,
+        'grade_value' => 9.4,
+        'grade_type' => 'semester_grade',
+        'grade_inputs' => {
+          'semester_id' => @semester.id
+        }
       }
-    }
-    b = Builder.new(inputs)
-    b.perform
-    grade = StudentSemesterGrade.first
+    end
 
-    expect(StudentSemesterGrade.count).to eq(1)
+    it 'create a new grade' do
+      builder = GradeBuilder.new(@inputs)
+      builder.perform
+      grade = StudentSemesterGrade.first
+
+      expect(StudentSemesterGrade.count).to eq(1)
+      expect(grade.value).to eq(9.4)
+      expect(grade.student).to eq(@student)
+      expect(grade.subject).to eq(@subject)
+    end
+
+    it 'update existent grade' do
+      grade = create(
+        :student_semester_grade,
+        student: @student,
+        semester: @semester,
+        subject: @subject,
+        value: 8.6
+      )
+      builder = GradeBuilder.new(@inputs)
+      builder.perform
+      grade.reload
+
+      expect(StudentSemesterGrade.count).to eq(1)
+      expect(grade.value).to eq(9.4)
+    end
   end
 end
